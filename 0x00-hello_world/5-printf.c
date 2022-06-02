4774e0a2f5d4a587fa062c8c4899e33c80e1f1b2
Elias Shamil <eliasshamil@gmail.com>
2022-06-02 13:04:02 -0700

    c 0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - prints the phrase with printf
+ * Return: 0 to exit properly
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
