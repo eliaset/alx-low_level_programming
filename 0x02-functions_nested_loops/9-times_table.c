a0af067883c9750ad4ae7bc2b30c06253ce948e6
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 11:44:15 -0700

    shell 2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,24 +3,21 @@
 /**
 * times_table - multiplication
 */
-
-void times_table(void)
+ 
+void times_table(void);
 {
 int i, j, k;
 
-	for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			k = i * j;
-			_putchar (k / 10);
-			_putchar (k % 10);
-			if (i != 9 && j != 9)
-			{
-				_putchar (',');
-				_putchar (' ');
-			}
-		}
-	_putchar ('\n');
+		k = i * j;
+		_putchar ((k / 10) + '0');
+		_putchar ((k % 10) + '0');
+		_putchar (',');
+		_putchar (' ');
 	}
+	_putchar ('\n');
+}
 }
