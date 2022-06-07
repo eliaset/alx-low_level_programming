44e29beb4808a5d37ef81e529039b7575eea0448
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 07:57:46 -0700

    shell 2

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - absolute number
+ * @n: place holder
+ * Return: n return the value
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
