a497cdc9fd4cb2f86e0c049d10ceb1f648daf22b
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 06:02:28 -0700

    shell 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - check if alphabet is lower case
+ */
+
+int _islower(int c)
+{
+	if (n > 97 && n < 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
