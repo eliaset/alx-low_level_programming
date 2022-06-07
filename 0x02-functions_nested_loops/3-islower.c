5357eed590b683eea09c28912d979776c92f7fc2
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 06:07:15 -0700

    shell 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,15 @@
 #include "main.h"
 /**
  * _islower - check if alphabet is lower case
+ * c - place holder
+ *Return: 0 1 to exit
+ *
  */
 
 int _islower(int c)
 {
+	int n;
+
 	if (n > 97 && n < 122)
 	{
 		return (1);
