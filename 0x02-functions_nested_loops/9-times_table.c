d6375b630b94794387b487e28813f9e5942824cd
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 10:27:48 -0700

    shell 2

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* times_table - multiplication
+*/
+
+void times_table(void);
+{
+int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			_putchar ((k / 10) + '0');
+			_putchar ((k % 10) + '0');
+			_putchar (',');
+			_putchar (' ');
+			_putchar ('\n');
+		}
+	}
+}
