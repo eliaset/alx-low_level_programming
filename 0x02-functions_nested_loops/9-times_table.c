4a3f76f1f5c079c7557336d664a78f73cb4f72a7
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 11:53:13 -0700

    shell 2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ for (i = 0; i < 10; i++)
 		k = i * j;
 		_putchar ((k / 10) + '0');
 		_putchar ((k % 10) + '0');
-		if (i != 9 && j != 9)
+		if (i != 9 || j != 9)
 		{
 			_putchar (',');
 			_putchar (' ');
