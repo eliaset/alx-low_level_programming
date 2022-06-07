3df56bf148f40b81fc038cde4d1c512ed8e172b4
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 00:36:50 -0700

    shell 2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the string
+ * Return: 0 exit the code
+ */
+
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+	return (0);
+}
