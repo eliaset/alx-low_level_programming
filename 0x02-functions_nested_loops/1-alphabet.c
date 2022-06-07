d28bd12a0ef148908352e6eddcb63edf2ccc88d3
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 04:51:39 -0700

    shell 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
  /**
   * print_alphabet - function from user
-  * Return: 0 exit code
   */
 void print_alphabet(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-        {
-                _putchar (i);
-        }
-        _putchar('\n');
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar (i);
+	}
+	_putchar('\n');
 }
