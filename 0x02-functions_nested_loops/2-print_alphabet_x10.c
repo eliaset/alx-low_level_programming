586aa7679d9ad70d08b0cdf4e7d492a780c0e392
Elias Shamil <eliasshamil@gmail.com>
2022-06-07 05:16:04 -0700

    shell 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = 97; j <= 122; j++)
+		{
+			_putchar (j);
+		}
+	}
+}
