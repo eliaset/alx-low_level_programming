2e3144315d4cfbd740c89e71807265d159315724
Elias Shamil <eliasshamil@gmail.com>
2022-06-09 11:29:24 -0700

    c 4

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints Buzz, Fizz, or FizzBuzz 
+ * for multiple of 3 and 5 and 3*5.
+ * Return: 0 to exit
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %i", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
