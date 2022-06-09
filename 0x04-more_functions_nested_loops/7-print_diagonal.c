bdcc4b4c9cede61187b14c0172373e163b730422
Elias Shamil <eliasshamil@gmail.com>
2022-06-09 10:11:40 -0700

    c 4

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,10 +15,11 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < i; j++)
 			{
-				_putchar (' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
-			_putchar ('\\');
-			_putchar ('\n');
 		}
 		_putchar ('\n');
 	}
