e7390edabeceb55941fa98acfddf816c7255eb67
Elias Shamil <eliasshamil@gmail.com>
2022-06-09 08:32:25 -0700

    c 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 to 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int i = 0, j = 0;
+
+	while (i <= 10)
+	{
+		while (j <= 14)
+		{
+			_putchar (j / 10);
+			_putchar (j % 10);
+			j++;
+		}
+		i++;
+		_putchar ('\n');
+	}
+}
