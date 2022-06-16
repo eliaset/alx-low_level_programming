7e2986d7d284aea5f65eae4ec6ae854cc7443328
Elias Shamil <eliasshamil@gmail.com>
2022-06-16 11:16:13 -0700

    c 6

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int m = strlen(src);
-	int p = strlen(dest);
+	int i, j;
 
-	for (int i = 0; i <= m; i++)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
 	{
-		dest[i + p] = src[i];
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
+
+	dest[i] = '\0';
+
 	return (dest);
 }
