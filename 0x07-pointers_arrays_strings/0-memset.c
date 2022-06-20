e2ab1f90bcfd8005f4345ffb36cde5675d6defad
Elias Shamil <eliasshamil@gmail.com>
2022-06-20 04:00:37 -0700

    c 3

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
+	unsigned int i = 0;
+	while (i < n)
 	{
-		s[i] = b;
+			s[i] = b;
+			i++;
 	}
-
-	return (s);
+        return (s);
 }
