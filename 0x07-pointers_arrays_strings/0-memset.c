cdfb125ec5258c6a704fd9498debad740dbda094
Elias Shamil <eliasshamil@gmail.com>
2022-06-20 02:32:11 -0700

    c 7

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,12 @@
  * @s: the address of memory to filled
  * @n: the size of the memory to filled
  * @b: the constant value to be filled
- * Return: Nothing.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
+	i = 0;
 
 	while (i < n)
 	{
