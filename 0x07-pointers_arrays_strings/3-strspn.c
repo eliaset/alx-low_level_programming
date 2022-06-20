fb048dc64cc98632881d0b49496e3ad77f09aef2
Elias Shamil <eliasshamil@gmail.com>
2022-06-20 10:19:27 -0700

    c 7

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,29 +7,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
-	int k;
-	int l;
+	int i;
+	int j;
+	int c = 0;
 	int n = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; i < accept[i] != '\0'; i++)
 	{
-		i++;
-	}
-	while (accept[j] != '\0')
-	{
-		j++;
-	}
-	for (k = 0; k < j; k++)
-	{
-		for (l = 0; l < i; l++)
+		for (j = 0; j < s[j] != '\0'; j++)
 		{
-			if (accept[k] == s[l])
+			if (accept[i] == s[j])
 			{
 				n++;
+				c = 1;
 			}
 		}
+		if (c == 0)
+			return (n);
 	}
 	return (n);
 }
