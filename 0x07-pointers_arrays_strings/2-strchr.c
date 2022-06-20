c27c59ad4dd06a53aae73209b3aacfe93fbb0cd9
Elias Shamil <eliasshamil@gmail.com>
2022-06-20 08:17:13 -0700

    c 7

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,9 +11,15 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
+	{
 		if (s[i] == c)
+		{
 			return (s + i);
-	if (s[i] == c)
-		return (s + i);
-	return ('\0');
+		}
+	
+		else if (s[i] != c && s[i] == '\0')
+			{
+				return (NULL);
+			}
+	}
 }
