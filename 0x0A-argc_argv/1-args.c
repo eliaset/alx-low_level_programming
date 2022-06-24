aa1bfc6a659e1a723cf8d2f5d381f085b19e58c4
Elias Shamil <eliasshamil@gmail.com>
2022-06-24 03:18:53 -0700

    c A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main"
+#include <stdio.h>
+/**
+ * main - prints its number of arguments
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguements
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
