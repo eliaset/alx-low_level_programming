7c462488110d3460b8a6274b285b1a83aefe9552
Elias Shamil <eliasshamil@gmail.com>
2022-06-24 09:08:10 -0700

    c A

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int k = 0;
+	int c = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (((argv[i][j]) < '0' || (argv[i][j] > '9')) && (argv[i][j] != '-'))
+			{
+				c++;
+			}
+		}
+		k += atoi(argv[i]);
+	}
+	if (c == 0)
+	{
+		printf("%d\n", k);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
