4a5de812e4bea8223fc97266cb1dc1ede5bf5eb0
Elias Shamil <eliasshamil@gmail.com>
2022-06-24 04:38:23 -0700

    c A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,19 +7,16 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	int i;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	i = atoi(argv[1]) * atoi(argv[2]);
-	if (argc == 3)
+	else
 	{
-		printf("%d\n", i);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 }
