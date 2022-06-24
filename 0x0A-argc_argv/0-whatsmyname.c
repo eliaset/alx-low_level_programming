438d9edcdb5196b82ba10e86e548eb47b531775e
Elias Shamil <eliasshamil@gmail.com>
2022-06-24 03:06:54 -0700

    c A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,12 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	arg c = 0;
 
-	for (i = 0; argv[0][i] != '\0'; i++)
-	{
-		_putchar (argv[0][i]);
-	}
-	_putchar ('\n');
-		return (0);
+	printf("%s\n", argv[argc]);
+	return (0);
 }
