0999cffd2b06e34c2a6582023133545765ee9939
Elias Shamil <eliasshamil@gmail.com>
2022-06-24 03:09:12 -0700

    c A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints its name + \n
  * @argc: number of arguments typed
  * @argv: array pointing to arguements
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	arg c = 0;
+	argc = 0;
 
 	printf("%s\n", argv[argc]);
 	return (0);
