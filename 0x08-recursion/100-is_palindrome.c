e0a4b00dd5191bd188abecd5f98ebd2e462dfc25
Elias Shamil <eliasshamil@gmail.com>
2022-06-27 09:28:17 -0700

    c 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,46 @@
 #include "main.h"
+
 /**
- * is_palindrome-  Returns the natural square root of a number.
- * @s: int type
- * Return: char type
- **/
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
+
+/**
+ * _strlen - find length of string to access last index
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+
+/**
+ * is_palindrome - check if palindrome
+ * @s: string to check
+ * Return: 1 if palindrome, 0 if not
+ */
+
 int is_palindrome(char *s)
 {
-	return;
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen(s)));
 }
