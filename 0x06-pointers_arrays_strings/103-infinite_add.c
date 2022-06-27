46995138ec65ecac0257a7cbe6f1b59bb5260d29
Elias Shamil <eliasshamil@gmail.com>
2022-06-27 10:12:12 -0700

    c 6

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
+ *
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (0);
+}
