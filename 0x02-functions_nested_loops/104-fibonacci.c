8eb530f77b47263e8b8a85e97bec63ae802141cf
Elias Shamil <eliasshamil@gmail.com>
2022-06-27 08:40:47 -0700

    c 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,25 +8,40 @@
  */
 int main(void)
 {
-	long int i, j, k, n;
+	unsigned long int i, j, j1, j2, k, k1, k2, n, n1, n2;
 
 	j = 1;
-
 	k = 2;
-
-	for (i = 1; i <= 91; ++i)
+	for (i = 1; i <= 90; i++)
 	{
 		if (i == 1)
 		{
-			printf("%ld", j);
+			printf("%lu", j);
 		}
 		else
 		{
-			printf(", %ld", j);
+			printf(", %lu", j);
 		}
 		n = j + k;
 		j = k;
 		k = n;
+	}
+	j1 = j / 10000000000;
+	j2 = j % 10000000000;
+	k1 = k / 10000000000;
+	k2 = k % 10000000000;
+	n1 = n / 10000000000;
+	n2 = n % 10000000000;
+	for (i = 91; i <= 98; i++)
+	{
+		printf(", %lu", j1);
+		printf("%lu", j2);
+		n1 = k1 + j1;
+		j1 = k1;
+		k1 = n1;
+		n2 = k2 + j2;
+		j2 = k2;
+		k2 = n2;
 	}
 	printf("\n");
 	return (0);
