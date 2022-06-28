d28e9e3c764189124b5323618dc25867346a0251
Elias Shamil <eliasshamil@gmail.com>
2022-06-28 13:46:01 -0700

    c B

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,44 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * *argstostr - concatenates all the arguments of the program
- * @ac: number of arguments
- * @av: array of arguments
- *
- * Return: Pointer to the new string (Success), NULL (Error)
+ * argstotmp - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
  */
-char *argstostr(int ac, char **av)
+
+char *argstotmp(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+	char *tmp;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	for (i = 0; i < ac; i++)
+	while (a < ac)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-
-	str = malloc(sizeof(char) * len);
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		b = 0;
+		while (av[a][b] != '\0')
 		{
-			str[k] = av[i][j];
-			k++;
+			count++;
+			b++;
 		}
-		str[k] = '\n';
-		k++;
+		a++;
 	}
-
-	return (str);
+	count = count + ac + 1;
+	tmp = malloc(sizeof(char) * count);
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			tmp[c] = av[a][b];
+			c++;
+		}
+		tmp[c] = '\n';
+		c++;
+	}
+	return (tmp);
 }
