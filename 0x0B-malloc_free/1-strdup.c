6e5129b0781c811babf621d8e0aae642a43d2cc6
Elias Shamil <eliasshamil@gmail.com>
2022-06-28 10:41:19 -0700

    c B

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *tmp;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j > 0)
+	{
+		tmp = malloc(sizeof(char) * j);
+		if (tmp == NULL)
+		{
+			return ('\n');
+		}
+		else
+		{
+			for (i = 0; i < j; i++)
+			{
+				tmp[i] = str[i];
+			}
+			tmp[i] = '\0';
+			return (tmp);
+			free(tmp);
+		}
+	}
+	else
+	{
+		return ('\0');
+	}
+}
