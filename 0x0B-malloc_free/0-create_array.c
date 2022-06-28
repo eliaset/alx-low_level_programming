6e20fc5c7d8ec996bbef5819df603f91157009ca
Elias Shamil <eliasshamil@gmail.com>
2022-06-28 10:17:18 -0700

    c B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,15 +14,22 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *tmp;
 
-	tmp = malloc(sizeof(char) * size);
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		tmp = malloc(sizeof(char) * size);
+		if (tmp == NULL)
 		{
-			tmp[i] = c;
+			return ('\n');
 		}
-		return (tmp);
 	}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				tmp[i] = c;
+			}
+			return (tmp);
+		}
 	else if (size == 0)
 	{
 		return ('\0');
