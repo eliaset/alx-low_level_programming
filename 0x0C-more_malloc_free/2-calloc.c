9ee73f9e164e8d0671364b127278be0b24313ee0
Elias Shamil <eliasshamil@gmail.com>
2022-06-30 07:07:03 -0700

    cC

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc -  allocate memory for an array, using malloc
+ * @nmemb: an array of size bytes
+ * @size: no of bytes
+ * Return: pointer to the new string created (Success), or NULL (Error)
+ **/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *tmp;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	tmp = malloc(sizeof(char) * nmemb + 1);
+	if (tmp == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		tmp[i] = 0;
+	return (tmp);
+}
