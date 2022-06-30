9f4e9406265f7aa97c21640fc1488dfe560a34a1
Elias Shamil <eliasshamil@gmail.com>
2022-06-30 05:03:49 -0700

    c C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: integer
+ * Return: pointer to grid or null
+ **/
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *tmp;
+
+	tmp = malloc(sizeof(int) * b);
+	if (tmp == NULL)
+	{
+		return (98);
+	}
+	return (tmp);
+}
