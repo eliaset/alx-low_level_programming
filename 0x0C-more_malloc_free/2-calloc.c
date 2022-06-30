9473834fb89d415cd7ea7f1a905db1e10ad00daf
Elias Shamil <eliasshamil@gmail.com>
2022-06-30 07:37:02 -0700

    c C

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	tmp = malloc(nmemb);
+	tmp = malloc(nmemb * size);
 	if (tmp == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
