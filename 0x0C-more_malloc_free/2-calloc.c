7b87fd3d30ad0da2f246b2b1daa59e171f60119a
Elias Shamil <eliasshamil@gmail.com>
2022-06-30 07:31:20 -0700

    c C

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,8 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	tmp = malloc(nmemb * size);
 	if (tmp == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		tmp[i] = 0;
 	return (tmp);
-	free(tmp);
 }
