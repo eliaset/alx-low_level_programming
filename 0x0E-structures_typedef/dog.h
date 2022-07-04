7902489f5d13d85e46933ab8dab3ac72ff1e361d
Elias Shamil <eliasshamil@gmail.com>
2022-07-04 03:11:33 -0700

    c E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure name dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ */
 struct dog
 {
 	char *name;
