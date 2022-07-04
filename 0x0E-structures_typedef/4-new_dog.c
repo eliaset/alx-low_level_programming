c483fed09d7027fc7b5492b843eae4f6a3e2c743
Elias Shamil <eliasshamil@gmail.com>
2022-07-04 11:08:50 -0700

    c E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,30 +9,30 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int tmp1, tmp2, i;
+	int nlen, olen, i;
 	dog_t *doggy;
 
-	tmp1 = tmp2 = 0;
-	while (name[tmp1++])
+	nlen = olen = 0;
+	while (name[nlen++])
 		;
-	while (owner[tmp2++])
+	while (owner[olen++])
 		;
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
 
-	doggy->name = malloc(tmp1 * sizeof(doggy->name));
+	doggy->name = malloc(nlen * sizeof(doggy->name));
 	if (doggy == NULL)
 		return (NULL);
-	for (i = 0; i < tmp1; i++)
+	for (i = 0; i < nlen; i++)
 		doggy->name[i] = name[i];
 
 	doggy->age = age;
 
-	doggy->owner = malloc(tmp2 * sizeof(doggy->owner));
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
 	if (doggy == NULL)
 		return (NULL);
-	for (i = 0; i < tmp2; i++)
+	for (i = 0; i < olen; i++)
 		doggy->owner[i] = owner[i];
 	return (doggy);
 }
