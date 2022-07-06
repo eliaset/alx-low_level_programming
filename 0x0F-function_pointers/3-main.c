c27109eeea826ec6204c07755982f13d8c925f8f
Elias Shamil <eliasshamil@gmail.com>
2022-07-06 08:55:35 -0700

    c F

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations (calculator)
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Always)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*cal)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	cal = get_op_func(argv[2]);
+
+	if (cal == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", cal(a, b));
+	return (0);
+}
