48635921a9294bb61e8ce48ce95e890a5bcc6114
Elias Shamil <eliasshamil@gmail.com>
2022-07-06 03:42:02 -0700

    c F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that takes pointer to function
+ * @name: name of the person
+ * @f: pointer to function
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
