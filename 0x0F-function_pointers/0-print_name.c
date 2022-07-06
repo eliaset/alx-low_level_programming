ad0add09103fdf6b84ed95ec221dda04b1411783
Elias Shamil <eliasshamil@gmail.com>
2022-07-06 03:48:41 -0700

    c F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-
 /**
  * print_name - function that takes pointer to function
  * @name: name of the person
@@ -10,5 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
+	return;
 }
