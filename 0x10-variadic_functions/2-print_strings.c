162dcc275a27e9926b954a6f98ca17ece96955fd
Elias Shamil <eliasshamil@gmail.com>
2022-07-07 10:57:05 -0700

    c 10

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_sings - prints sings.
+ * @separator: sing to be printed between the sings.
+ * @n: number of sings passed to the function.
+ *
+ * Return: no return.
+ */
+void print_sings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+
+		if (s != NULL)
+			printf("%s", s);
+		else
+			printf("(nil)");
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
