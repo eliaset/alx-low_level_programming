ce09c02a553fd4d6273a53e483256c1a2d034fdc
Elias Shamil <eliasshamil@gmail.com>
2022-07-10 09:42:27 -0700

    c F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
+	int (*cal)(int, int);
 
 	if (argc != 4)
 	{
@@ -23,21 +24,21 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
+	cal = get_op_func(op);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (cal == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", cal(num1, num2));
 
 	return (0);
 }
