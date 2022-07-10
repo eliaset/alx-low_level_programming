6dc6005c73debe6f3ce6b5ec043ad909bb021408
Elias Shamil <eliasshamil@gmail.com>
2022-07-10 09:23:04 -0700

    c F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
+	int a, b, result;
 	char o;
 	int (*func)(int, int);
 
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
 
@@ -34,13 +34,13 @@ int main(int argc, char *argv[])
 
 	o = *argv[2];
 
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((o == '/' || o == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
+	result = func(a, b);
 
 	printf("%d\n", result);
 
