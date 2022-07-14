cc261620db54f662fb08e9e28c334d5835387905
Elias Shamil <eliasshamil@gmail.com>
2022-07-14 09:59:21 -0700

    c 12

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,6 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	int c = 0;
 	list_t *tmp;
 
 	tmp = malloc(sizeof(list_t));
@@ -23,5 +22,5 @@ list_t *add_node(list_t **head, const char *str)
 	tmp->len = strlen(str);
 	tmp->next = *head;
 	*head = tmp;
-	return (head);
+	return (*head);
 }
