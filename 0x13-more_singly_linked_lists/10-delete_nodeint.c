d907b6330e31c5069df7016e06b7825ff7bbcac7
Elias Shamil <eliasshamil@gmail.com>
2022-07-18 09:40:23 -0700

    c 13

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - inserts a new node at a given position
+ * @head: head of linked list
+ * @index: index of the list where the new node should be added and starts at 0
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr = *head;
+	listint_t *tmp = *head;
+	unsigned int c = 1;
+
+	if (*head == NULL)
+		return (-1);
+	else if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		tmp = tmp->next;
+		while (ptr != NULL)
+		{
+			c++;
+			ptr = ptr->next;
+			tmp = tmp->next;
+			if (c == index)
+			{
+				ptr->next = tmp->next;
+				free(tmp);
+				return (1);
+			}
+		}
+	}
+	return (-1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp, *ptr;
-	int c = 1;
+	unsigned int c = 1;
 
 	if (*head == NULL)
 		return (NULL);
