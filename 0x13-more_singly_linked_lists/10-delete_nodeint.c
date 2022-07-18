7ea2e473467b3aa353b9c4b354dbfe665d75e9cf
Elias Shamil <eliasshamil@gmail.com>
2022-07-18 12:27:21 -0700

    c 13

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *ptr = *head;
 	listint_t *tmp = *head;
-	unsigned int c = 1;
+	unsigned int c = 0;
 
 	if (*head == NULL)
 		return (-1);
@@ -29,14 +29,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		while (ptr != NULL)
 		{
 			c++;
-			ptr = ptr->next;
-			tmp = tmp->next;
 			if (c == index)
 			{
 				ptr->next = tmp->next;
 				free(tmp);
 				return (1);
 			}
+			ptr = ptr->next;
+			tmp = tmp->next;
 		}
 	}
 	return (-1);
