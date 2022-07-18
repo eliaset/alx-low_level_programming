663c0b760319a31460cc165ee35fff90290d86e6
Elias Shamil <eliasshamil@gmail.com>
2022-07-18 11:11:44 -0700

    c 13

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -14,10 +14,10 @@ int sum_listint(listint_t *head)
 
 	if (head == NULL)
 		return (0);
-	while (head->next != NULL)
+	while (head != NULL)
 	{
+		sum += head->n;
 		head = head->next;
-		sum = sum + head->n;
 	}
 	return (sum);
 }
