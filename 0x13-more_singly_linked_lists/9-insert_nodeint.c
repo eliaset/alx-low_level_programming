bb8ddeb8879eaf0eaefbd9c0496171c45781ca0d
Elias Shamil <eliasshamil@gmail.com>
2022-07-18 12:15:41 -0700

    c 13

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = tmp;
 		return (tmp);
 	}
-	while (ptr != NULL && c < idx)
+	while (ptr != NULL)
 	{
 		c++;
 		if (c == idx)
