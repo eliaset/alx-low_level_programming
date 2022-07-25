cc9b8cbd53758db7435f0426444b3a67625ae7a9
Elias Shamil <eliasshamil@gmail.com>
2022-07-25 09:53:07 -0700

    c 15

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int c = 0;
+	int w;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+	while (text_content[c] != '\0')
+		c++;
+	w = write(fd, text_content, c);
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
