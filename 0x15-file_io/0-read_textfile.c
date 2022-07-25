4800df5ddec4b44a8a5d5200b73e6322c5995e6b
Elias Shamil <eliasshamil@gmail.com>
2022-07-25 04:39:41 -0700

    c 15

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int fd;
+	int m = 0;
+	int n = 0;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf != NULL)
+		return (0);
+	read(fd, buf, letters);
+	buf[letters] = '\0';
+	close(fd);
+
+	fd = open(filename, O_CREAT | O_WRONLY);
+	if (fd == -1)
+		return (0);
+	write(fd, filename, letters);
+	close(fd);
+
+	while (buf[n] != 0)
+	{
+		if (buf[n] != 32)
+		{
+			m++;
+		}
+		n++;
+	}
+	return (m);
+}
