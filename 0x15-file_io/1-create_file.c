856a2d4150859e9dfe5369eedd02be222b7798b6
Elias Shamil <eliasshamil@gmail.com>
2022-07-25 09:41:20 -0700

    c 15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
 	while (text_content[c] != '\0')
 		c++;
 	w = write(fd, text_content, c);
