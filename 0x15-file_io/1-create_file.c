96f33cbdf42096de17ddd6db5481e3c62ee13b8a
Elias Shamil <eliasshamil@gmail.com>
2022-07-25 09:36:33 -0700

    c 15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,10 +9,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
+	int c;
+	int w;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -20,18 +20,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (c = 0; text_content[c]; c++)
 		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	w = write(fd, text_content, c);
+	if (w == -1)
 		return (-1);
-
 	close(fd);
-
 	return (1);
 }
