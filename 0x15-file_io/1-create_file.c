cf30fc3cdf6d971f605491853d23ab6e52f61f27
Elias Shamil <eliasshamil@gmail.com>
2022-07-25 09:15:56 -0700

    c 15

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int c = 0;
+	int n = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	while(text_content[c] != NULL)
+	{
+		c++;
+	}
+	write(fd, filename, c);
+	close(fd);
+	return (1);
+}
