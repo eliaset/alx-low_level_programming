266b9b0048fe650df7b7ca5082feaf877f3aa91b
Elias Shamil <eliasshamil@gmail.com>
2022-07-25 06:24:13 -0700

    c 15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,24 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
 	char *buf;
+	int fd;
+	ssize_t r, w;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
+	r = read(fd, buf, letters);
+	buf[letters] = '\0';
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	w = write(STDOUT_FILENO, buf, r);
 	close(fd);
-
 	free(buf);
 
-	return (nwr);
+	return (w);
 }
