2d5732bc1b1c2e7a4f9bf920f7d05b74603aeae6
Elias Shamil <eliasshamil@gmail.com>
2022-09-28 10:43:17 -0700

    c 1A

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -58,7 +58,6 @@ typedef struct shash_node_s
 	struct shash_node_s *snext;
 } shash_node_t;
 
-
 /**
  * struct shash_table_s - Sorted hash table data structure
  * @size: The size of the array
@@ -81,5 +80,6 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht)
-#endif
+void shash_table_delete(shash_table_t *ht);
+
+#endif /* HASH_TABLES_H */
